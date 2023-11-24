96890d3fe23bb1a5549155e0c0512868b061622e
Nick Vermonden <nick@newfront.be>
2023-11-24 13:59:35 +0100

    target sveltekit path for hero img bg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
-<section class="bg-[url('./src/lib/assets/img/barbecue-peter-bbq-homebg.jpg')] bg-cover py-12">
+<section class="bg-[url('$lib/assets/img/barbecue-peter-bbq-homebg.jpg')] bg-cover py-12">
 jo
 </section>
