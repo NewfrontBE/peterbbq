6a419b2254c72cc046a147e90290b2187bbe219d
Nick Vermonden <nick@newfront.be>
2023-12-11 14:56:09 +0100

    add bg images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,14 @@
-<section class="bg-[url('$lib/assets/img/barbecue-peter-bbq-homebg.jpg')] bg-cover py-12">
-jo
+<svelte:head>
+	<title>Peter Barbecue - Cateraar voor al uw feesten!</title>
+	<meta name="description" content="Een culinaire barbecue aan huis of op locatie, laat u natuurlijk organiseren door een specialist in catering." />
+</svelte:head>
+
+<section class="bg-[url('$lib/assets/img/barbecue-peter-bbq-homebg.jpg')] bg-cover bg-center py-48 text-center">
+    <h2 class="text-6xl font-medium text-shadow yanone">Peter Barbecue</h2>
+    <p class="text-4xl text-shadow yanone">Uw feest is onze passie</p>
+</section>
+
+<section class="bg-[url('$lib/assets/img/home-inotrosection-bf.jpg')] bg-cover bg-center py-24 grid grid-cols-2 gap-16">
+    <div>1</div>
+    <div>2</div>
 </section>
---
+++ src/routes/fotos/+page.svelte
@@ -0,0 +1,4 @@
+<svelte:head>
+	<title>Peter Barbecue - Foto's</title>
+	<meta name="description" content="Een culinaire barbecue aan huis of op locatie, laat u natuurlijk organiseren door een specialist in catering." />
+</svelte:head>
